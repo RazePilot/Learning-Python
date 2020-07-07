50106f991821bb66685048ccf8cf9fef1416c86e
Lake Valente <36944020+RazePilot@users.noreply.github.com>
2020-07-07 17:44:43 -0600

    remake excercise 13

---
+++ ex13.py
@@ -0,0 +1,12 @@
+from sys import argv
+
+script, first, second, third = argv # pylint: disable=unbalanced-tuple-unpacking 
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+
+fourth = raw_input("What is your name? ")
+
+print "Your name is %s" % fourth 
