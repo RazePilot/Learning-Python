5f15bb8dd531f07ecf9c1cb40ead4a98686c77a6
Lake Valente <36944020+RazePilot@users.noreply.github.com>
2020-07-07 17:35:50 -0600

    initialized

---
+++ clutter.py
@@ -0,0 +1,10 @@
+from sys import argv
+
+script, number = argv # pylint: disable=unbalanced-tuple-unpacking 
+
+for i in range(0, int(number)):
+    f= open("Clutter%d.txt" % i ,"w+") 
+    f.close()
+ 
+
+
---
+++ exmaker.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+string, i = argv # pylint: disable=unbalanced-tuple-unpacking 
+
+
+f= open("ex%s.py" % i ,"a") 
+f.close()
+
+
