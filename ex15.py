fffc19083c0ed1c60afdb6b311b64ea2cebe294b
Lake Valente <36944020+RazePilot@users.noreply.github.com>
2020-07-07 18:50:44 -0600

    ex15

---
+++ ex15.py
@@ -0,0 +1,24 @@
+# imports argv
+from sys import argv
+
+# applies filename to argv
+script, filename = argv # pylint: disable=unbalanced-tuple-unpacking 
+
+# assigns the variable 'txt' to open the argv
+txt = open(filename)
+
+# tells us what file we just opened
+print "Here's your file %r:" % filename
+# spits out the contents of that file
+print txt.read()
+
+# gives the user another instruction
+print "Type in the filename again:" 
+# gives the user a space to type the file name again 
+file_again = raw_input("> ")
+
+# makes the txt_again variable open the file from the above raw input 
+txt_again = open(file_again)
+
+# prints txt_again, which opens the file and then spits out its contents with .read
+print txt_again.read()
